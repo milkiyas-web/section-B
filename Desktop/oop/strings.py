1e2cde71a7101696b64a3ec00becaa567df3c2fe
Milkiyas <milkiyashailub@gmail.com>
2025-03-23 19:29:01 +0300

    strings:split()

--- Desktop/oop/strings.py
+++ Desktop/oop/strings.py
@@ -6,9 +6,11 @@ print(name.isupper())
 print(name.upper() + " " + last_name.upper())
 print(last_name.count("e"))
 
-## )03/21/ 2025
+## 03/21/ 2025
 
 print(name[0:2] + last_name[0:])
 text = "   Hello World   "
 print(text.strip())
+
+print(text.split())
 
