6bd47bb32cb90750c467bc8b2f113d1d88697952
Milkiyas <milkiyashailub@gmail.com>
2025-05-04 12:28:25 +0300

    set

--- Desktop/oop/set.py
+++ Desktop/oop/set.py
@@ -1,5 +1,6 @@
 s = {1, 2, 5, 100}
 s.add(3)
+
 s.remove(1)
 s.discard(5)
 
@@ -7,3 +8,9 @@ s.pop()
 s.clear()
 s2 = s.copy()
 print(s2)
+
+set1 = set([1, 2, 3])
+set2 = set([2, 3, 4])
+print(set1.difference(set2))
+
+
